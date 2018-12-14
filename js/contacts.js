6a265249395a34015c3966fa0cd90cf7d5d02cda
Gabriella G. R <g.rozenaite@gmail.com>
2018-12-14 11:53:11 +0100

    Add Contacts page

---
+++ js/contacts.js
@@ -0,0 +1,27 @@
+const contactsImg = document.getElementById("contacts__img");
+
+fetch("http://workshopab.com/2nd_exam/wordpress/wp-json/wp/v2/about/100?_embed")
+  .then(data => data.json())
+  .then( actor => {
+    console.log(actor._embedded["wp:featuredmedia"][0].media_details.sizes);
+    contactsImg.src = actor._embedded["wp:featuredmedia"][0].media_details.sizes.large.source_url;
+  })
+
+const contManager = document.getElementById("cont-manager");
+const contActor = document.getElementById("cont-actor");
+
+fetch("http://workshopab.com/2nd_exam/wordpress/wp-json/wp/v2/about")
+  .then(data => data.json())
+  .then(info => {
+    info.forEach(elem => {
+      if (elem.id === 100) {
+        contActor.querySelector(".contacts__phone").innerHTML = "<i class=\"fas fa-mobile-alt\"></i> " + " " + elem.acf.phone_number;
+        contActor.querySelector(".contacts__email").innerHTML = "<i class=\"fas fa-envelope\"></i> " + " " + elem.acf.email;
+      }
+      else if (elem.id === 143) {
+        contManager.querySelector("h2").innerHTML = elem.title.rendered;
+        contManager.querySelector(".contacts__phone").innerHTML = "<i class=\"fas fa-mobile-alt\"></i> " + " " + elem.acf.phone_number;
+        contManager.querySelector(".contacts__email").innerHTML = "<i class=\"fas fa-envelope\"></i> " + " " + elem.acf.email;
+      }
+    });
+  });
--- js/production-company.js
+++ js/production-company.js
@@ -1,3 +1,5 @@
+// Production Company
+// Dynamic text
 const teaxtAboutCompany = document.getElementById("teaxtAboutCompany");
 
 fetch("http://workshopab.com/2nd_exam/wordpress/wp-json/wp/v2/about/101")
@@ -5,3 +7,25 @@ fetch("http://workshopab.com/2nd_exam/wordpress/wp-json/wp/v2/about/101")
   .then( info => {
     teaxtAboutCompany.innerHTML = info.content.rendered;
   });
+
+// Dynamic gallery
+const tplThumbnail = document.getElementById("tplCompanyGalleryThumbnails").content;
+const imgThumbnailsParent = document.getElementById("mini-gallery");
+const imgPreview = document.getElementById("img__preview");
+
+fetch("http://workshopab.com/2nd_exam/wordpress/wp-json/wp/v2/umdieecke_posters?_embed")
+  .then(data => data.json())
+  .then(posters => {
+    posters.forEach(poster => {
+      const tplCopy = tplThumbnail.cloneNode(true);
+      let posterImg = poster._embedded["wp:featuredmedia"][0].media_details.sizes.medium_large.source_url;
+      changeImgPreview(posterImg);
+      tplCopy.querySelector(".img-thumbnail").src = posterImg;
+      tplCopy.querySelector(".img-thumbnail").addEventListener("click", () => changeImgPreview(posterImg));
+      imgThumbnailsParent.appendChild(tplCopy);
+    })
+  });
+function changeImgPreview(path) {
+  console.log(path);
+  imgPreview.src = path;
+}
