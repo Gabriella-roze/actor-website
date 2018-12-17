d7ce8ada0aab10272b4888a96a27aa8c21b1606d
Gabriella G. R <g.rozenaite@gmail.com>
2018-12-17 22:01:04 +0100

    Add titles to mini gallery in AboutMe page

--- js/index.js
+++ js/index.js
@@ -17,6 +17,7 @@ const demoVideoFour = document.getElementById("demoVideoFour");
 const video = document.getElementById("video");
 const picture = document.getElementById("picture");
 const intVideo = document.getElementById("intVideo");
+const previewTitle = document.getElementById("preview__title");
 
 
 demoVideoOne.addEventListener('click', () => {previewImage("one", demoVideoOne)});
@@ -32,21 +33,25 @@ function previewImage(image, btnClicked) {
   console.log('image-btn: ', image, btnClicked)
   btnClicked.classList.add("highlighted");
   if (image === "one") {
+    previewTitle.innerHTML = "Dan Zahle Showreel 2017";
     picture.style.display = "none";
     intVideo.style.display = "none";
     video.src = "https://www.youtube.com/embed/y8QSy5TR4wY";
     video.style.display = "initial";
   } else if (image === "two") {
+    previewTitle.innerHTML = "Interview with Dan Zahle by KEA students";
     picture.style.display = "none";
     video.style.display = "none";
     intVideo.src = "interview_video.mp4";
     intVideo.style.display = "initial";
   } else if (image === "three") {
+    previewTitle.innerHTML = "Dan Zahle headshot";
     video.style.display = "none";
     intVideo.style.display = "none";
     picture.src = "images/actor-dan-zahle_headshot.jpg";
     picture.style.display = "initial";
   } else {
+    previewTitle.innerHTML = "Dan Zahle at work - theatre";
     video.style.display = "none";
     intVideo.style.display = "none";
     picture.src = "images/actor-dan-zahle_headshot2.png";
